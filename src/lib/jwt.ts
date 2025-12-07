717612066fd2483d45faab7cab3a06fc95e63bc1
Rahmad Iqbal <azcodia38@gmail.com>
2025-12-07 12:07:16 +0700

    adding jwt

---
+++ src/lib/jwt.ts
@@ -0,0 +1,13 @@
+// lib/jwt.ts
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
+
+export const verifyToken = (token: string): string => {
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+    return decoded.userId;
+  } catch (err) {
+    throw new Error("Invalid token");
+  }
+};
