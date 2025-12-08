e0aea6098df28d524fa9b5310954a5681521628c
Rahmad Iqbal <azcodia38@gmail.com>
2025-12-08 10:31:36 +0700

    refactor: update Color schema with updated_at and unique index

--- src/models/Color.ts
+++ src/models/Color.ts
@@ -1,19 +1,22 @@
-import mongoose, { Schema, Document, Model } from "mongoose";
+import { Schema, Document, models, model } from "mongoose";
 
 export interface IColor extends Document {
   color: string;
   created_at: Date;
+  updated_at: Date;
 }
 
 const ColorSchema: Schema<IColor> = new Schema(
   {
     color: { type: String, required: true, unique: true },
     created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now },
   },
   { collection: "colors" }
 );
 
-const Color: Model<IColor> =
-  mongoose.models.Color || mongoose.model<IColor>("Color", ColorSchema);
+ColorSchema.index({ color: 1 }, { unique: true });
 
-export default Color;
+const ColorModel = models.Color || model<IColor>("Color", ColorSchema);
+
+export default ColorModel;
