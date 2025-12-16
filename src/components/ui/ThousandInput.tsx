9d6d6dace0be6eb615038a235d31f773aa0122b7
Rahmad Iqbal <azcodia38@gmail.com>
2025-12-16 08:33:56 +0700

    feat(ui): add ThousandInput component for formatted number input

---
+++ src/components/ui/ThousandInput.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import { NumericFormat } from "react-number-format";
+
+interface ThousandInputProps {
+  label?: string;
+  value: string | number;
+  onChange?: (value: string) => void;
+  placeholder?: string;
+  error?: string;
+  disabled?: boolean;
+}
+
+const ThousandInput: React.FC<ThousandInputProps> = ({
+  label,
+  value,
+  onChange,
+  placeholder,
+  error,
+  disabled = false,
+}) => {
+  return (
+    <div className="flex flex-col w-full">
+      {label && <label className="text-gray-500 mb-1">{label}</label>}
+
+      <NumericFormat
+        value={value}
+        thousandSeparator=","
+        decimalScale={0} // Disable desimal
+        allowNegative={false} // Optional, kalau nggak mau angka negatif
+        placeholder={placeholder}
+        className={`
+          w-full p-2.5 rounded-md bg-grayd text-[#1f1f1f] border
+          ${error ? "border-danger" : "border-gray-100"}
+          focus:border-success-light focus:ring-1 focus:ring-success
+          focus:outline-none transition-all duration-200 shadow-sm
+          ${disabled ? "bg-gray-100 cursor-not-allowed" : ""}
+        `}
+        onValueChange={(vals) => onChange?.(vals.value)}
+        disabled={disabled}
+      />
+
+      {error && <p className="text-danger text-sm mt-1">{error}</p>}
+    </div>
+  );
+};
+
+export default ThousandInput;
