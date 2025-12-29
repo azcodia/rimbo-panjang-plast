d287f5ebd444afabfc58236bf23da273e72f888f
Rahmad Iqbal <azcodia38@gmail.com>
2025-12-29 14:25:36 +0700

    style(PageContainer): adjust content padding

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -9,7 +9,7 @@ const PageContainer: React.FC<PageContainerProps> = ({ title, children }) => {
       <div className="py-4 px-6 flex justify-between items-center border-b bg-grayd rounded-md border-gray-300 mb-4">
         <h1 className="text-lg font-semibold">{title}</h1>
       </div>
-      <div className="bg-grayd shadow-lg rounded-md p-4 space-y-3.5">
+      <div className="bg-grayd shadow-lg rounded-md p-2 space-y-3.5">
         {children}
       </div>
     </div>
