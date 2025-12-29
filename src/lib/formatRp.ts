1445bfa423110d4a40823eced84213802bbcb4b3
Rahmad Iqbal <azcodia38@gmail.com>
2025-12-29 14:31:39 +0700

    feat(utils): enhance formatRp with short and full format options

--- src/lib/formatRp.ts
+++ src/lib/formatRp.ts
@@ -1,17 +1,42 @@
+type FormatType = "short" | "full";
+
+interface FormatRpOptions {
+  format?: FormatType;
+  withSymbol?: boolean;
+}
+
 export function formatRp(
   value: number | string,
-  withSymbol: boolean = true
+  options: FormatRpOptions = {}
 ): string {
+  const { format = "full", withSymbol = true } = options;
   const numberValue = typeof value === "string" ? parseFloat(value) : value;
 
-  if (isNaN(numberValue)) {
-    return value.toString();
+  if (isNaN(numberValue)) return value.toString();
+
+  if (format === "full") {
+    const formatted = new Intl.NumberFormat("id-ID").format(numberValue);
+    return withSymbol ? `Rp.${formatted}` : formatted;
   }
 
-  const formatted = new Intl.NumberFormat("id-ID", {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(numberValue);
+  const shorten = (num: number, div: number, suffix: string) => {
+    const result = Math.floor((num / div) * 10) / 10;
+    return result % 1 === 0
+      ? `${result.toFixed(0)}${suffix}`
+      : `${result}${suffix}`;
+  };
+
+  let formatted = "";
+
+  if (numberValue >= 1_000_000_000) {
+    formatted = shorten(numberValue, 1_000_000_000, "M");
+  } else if (numberValue >= 1_000_000) {
+    formatted = shorten(numberValue, 1_000_000, "Jt");
+  } else if (numberValue >= 1_000) {
+    formatted = shorten(numberValue, 1_000, "Rb");
+  } else {
+    formatted = numberValue.toString();
+  }
 
   return withSymbol ? `Rp.${formatted}` : formatted;
 }
