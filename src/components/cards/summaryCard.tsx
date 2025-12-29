d4791eac05c768e9e1be78eb21e32eb8bac1202c
Rahmad Iqbal <azcodia38@gmail.com>
2025-12-29 14:30:48 +0700

    feat(ui): add loading state to SummaryCard

--- src/components/cards/summaryCard.tsx
+++ src/components/cards/summaryCard.tsx
@@ -1,24 +1,36 @@
+import LoadingSpinner from "../LoadingSpinner";
+
 interface SummaryCardProps {
   title: string;
   value: number | string | undefined;
   subtitle?: string;
+  loading?: boolean;
 }
 
 export default function SummaryCard({
   title,
   value,
   subtitle,
+  loading,
 }: SummaryCardProps) {
   return (
     <div className="rounded-md border bg-white p-4 shadow-sm">
-      <p className="text-sm text-gray-500">{title}</p>
-      <div className="mt-1 ml-2">
-        <p className="text-xl font-bold">{value ?? "-"}</p>
+      {loading ? (
+        <div className="flex justify-center items-center h-12">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <>
+          <p className="text-sm text-gray-500">{title}</p>
+          <div className="mt-1">
+            <p className="text-xl font-bold">{value ?? "-"}</p>
 
-        {subtitle && (
-          <p className="text-[10px] text-success font-bold">{subtitle}</p>
-        )}
-      </div>
+            {subtitle && (
+              <p className="text-[10px] text-success font-bold">{subtitle}</p>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
