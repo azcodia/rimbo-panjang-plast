9741858021fae54242c35acd8897b7b9fde29f59
Rahmad Iqbal <azcodia38@gmail.com>
2026-01-05 13:03:18 +0700

    refactor(customer): simplify DetailCustomerPage by removing tabs and dynamic imports

--- src/app/(protected)/dashboard/detail-customer/page.tsx
+++ src/app/(protected)/dashboard/detail-customer/page.tsx
@@ -3,32 +3,7 @@
 import { useSearchParams } from "next/navigation";
 import PageContainer from "@/components/PageContainer";
 import CustomerSummaryLayout from "./CustomerSummaryLayout";
-import Tabs, { TabItem } from "@/components/tabs/Tabs";
-import dynamic from "next/dynamic";
-import { ChartSpline, Table } from "lucide-react";
-import LoadingSpinner from "@/components/LoadingSpinner";
-
-const PaymentChartCustomer = dynamic(
-  () =>
-    import(
-      "@/app/(protected)/dashboard/detail-customer/components/customer/PaymentChart"
-    ).then((mod) => mod.default),
-  {
-    ssr: false,
-    loading: () => <LoadingSpinner />,
-  }
-);
-
-const PaymentTableCustomer = dynamic(
-  () =>
-    import(
-      "@/app/(protected)/dashboard/detail-customer/CustomerTransactionHistory"
-    ).then((mod) => mod.default),
-  {
-    ssr: false,
-    loading: () => <LoadingSpinner />,
-  }
-);
+import CustomerTransactionHistory from "@/app/(protected)/dashboard/detail-customer/CustomerTransactionHistory";
 
 export default function DetailCustomerPage() {
   const searchParams = useSearchParams();
@@ -45,25 +20,10 @@ export default function DetailCustomerPage() {
     );
   }
 
-  const tabs: TabItem[] = [
-    {
-      id: "table",
-      label: "Table Transaksi",
-      icon: <Table size={18} strokeWidth={2} />,
-      content: <PaymentTableCustomer customerId={customerId} />,
-    },
-    {
-      id: "payment",
-      label: "Grafik Pembayaran",
-      icon: <ChartSpline size={18} strokeWidth={2} />,
-      content: <PaymentChartCustomer customerId={customerId} />,
-    },
-  ];
-
   return (
     <PageContainer title={`Pelanggan: ${customerName ?? "-"}`}>
       <CustomerSummaryLayout customerId={customerId} />
-      <Tabs tabs={tabs} defaultTabId="table" />
+      <CustomerTransactionHistory customerId={customerId} />
     </PageContainer>
   );
 }
